7e6a0ad2abf65c25c8e545f058f8c87057ac26bf
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-23 09:57:38 +0300

    Proje dosyası ekle.

---
+++ Donguler/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  for dongusu ile ekrana 10 kez "merhaba dunya" yazdıralım;
+            //int i;
+
+            //for (i=1; i<=10; i++)
+            //{
+            //    Console.WriteLine(" *** Merhaba Dünya *** ");
+            //}
+
+
+            //For dongusu ile 1'den 10'a kadar olan sayıları yazdıralım
+            //int i;
+
+            //for (i = 1; i < 11; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //For dongusu ile 0-20 arasındaki çift sayıları yazdiralim
+            //int j;
+
+            //for (j = 0; j <= 20; j += 2)
+            //{
+            //    Console.WriteLine(j);
+            //}
+
+            //For dongusu ile 0-20 arası tek sayilari yazdiralim
+            //int j;
+
+            //for (j = 1; j < 20; j += 2)
+            //{
+            //    Console.WriteLine(j);
+            //}
+
+            //For dongusu ile ekrana 10 kez (bastaki sira numaralari degisecek sekilde) Hello world yazdiralim
+            int k;
+
+            for (k = 1; k < 11; k++)
+            {
+                Console.WriteLine(k + " Hello World ");
+            }
+           
+            Console.Read();
+
+        }
+    }
+}
